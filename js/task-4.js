702a3dceae168988decf0df2f631db56830370d9
onur uygun <onuruygun32@gmail.com>
2025-02-25 20:48:43 +0300

    Hatalar Düzeltildi

--- js/task-4.js
+++ js/task-4.js
@@ -16,22 +16,22 @@ function getShippingCost(country) {
     case "China":
       price = 100;
       return `Shipping to ${country} will cost ${price} credits`;
-      break;
+      
     case "Chile":
       price = 250;
       return `Shipping to ${country} will cost ${price} credits`;
-      break;
+      
     case "Australia":
       price = 170;
       return `Shipping to ${country} will cost ${price} credits`;
-      break;
-    case "Jameica":
+      
+    case "Jamaica":
       price = 120;
       return `Shipping to ${country} will cost ${price} credits`;
-      break;
+      
     default:
       return `Sorry, there is no delivery to your countr`;
-      break;
+      
   }
 }
 
